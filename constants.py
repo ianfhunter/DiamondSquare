70fd3341f77240fb8ccf6a6ea49ea66030fe279f
Ian Hunter <ianfhunter@gmail.com>
2015-02-24 18:59:57 +0000

    pygame display fix

--- constants.py
+++ constants.py
@@ -1,6 +1,12 @@
 initial_step = 1000
 max_height = 1000
-grid_size = 65
+grid_size =33               #Recommended Values: 9,17,33,65
 
 roughness = 1
-landscape_type = 1  #0 for hill , #1 for landscape
+landscape_type = 1  #0 for hill , anything else for landscape
+
+
+# 0 = csv coordinates (x,y,z)
+# 1 = 2D
+# 2 = 3D
+output_type = 2
--- main.py
+++ main.py
@@ -6,21 +6,11 @@ from utils import *
 from constants import *
 import sys
 
-##
-#
-#   Options - Positive increments only (hill)
-#
-#
-##
-
-#validate amount
+#validate input size.
 if (grid_size-1 & (grid_size - 2)) != 0:
-    sys.exit("Invalid Number. Must be power of 2 +1")
-
+    sys.exit("Invalid Number. Must be (2^n)+1 ")
 
 pp = pprint.PrettyPrinter(indent=4)
-depth = 0
-
 
 def MaxMinKeys(d):
      """ a) create a list of the dict's keys and values; 
@@ -30,17 +20,10 @@ def MaxMinKeys(d):
      return (k[v.index(max(v))],k[v.index(min(v))])
 
 #x,y - position in main grid
-def DSFractal(main,x,y,grid,depth,mx,my):
-    # print "Grid of size ", len(grid), "x", len(grid[0])
-    depth = depth + 1
+def DSFractal(main,x,y,grid,mx,my):
 
-    # print "main"
-    # pp.pprint(main)
     square(grid)
     diamond(main,x,y,grid)
-
-    # print "applied"
-    # pp.pprint(grid)
 
     minis = subgrid(grid)
 
@@ -52,20 +35,19 @@ def DSFractal(main,x,y,grid,depth,mx,my):
     #
     if len(minis[0][0]) > 2 and len(minis[0]) > 2 :
         insert(grid,main,mx,my)
-        insert(DSFractal(main,x,y,minis[0],depth,mx,my),grid,0,0)
+        insert(DSFractal(main,x,y,minis[0],mx,my),grid,0,0)
     
     if len(minis[1][0]) > 2 and len(minis[1]) > 2 :
         insert(grid,main,mx,my)
-        insert(DSFractal(main,(half)+x,y,minis[1],depth,mx+(half),my),grid,half,0)       
+        insert(DSFractal(main,(half)+x,y,minis[1],mx+(half),my),grid,half,0)       
 
     if len(minis[2][0]) > 2 and len(minis[2]) > 2 :
         insert(grid,main,mx,my)
-        insert(DSFractal(main,x,(half)+y,minis[2],depth,mx,my+(half)),grid,0,half) 
+        insert(DSFractal(main,x,(half)+y,minis[2],mx,my+(half)),grid,0,half) 
 
     if len(minis[3][0]) > 2 and len(minis[3]) > 2 :
         insert(grid,main,mx,my)
-        insert(DSFractal(main,(half)+x,(half)+y,minis[3],depth,mx+(half),my+(half)),grid,half,half)
-    
+        insert(DSFractal(main,(half)+x,(half)+y,minis[3],mx+(half),my+(half)),grid,half,half)   
 
     return grid
 
@@ -79,82 +61,93 @@ grid[len(grid)-1][0] =  max_height /2
 grid[len(grid)-1][len(grid)-1] = max_height /2 
 
 
-DSFractal(grid,0,0,grid,depth,0,0)
+DSFractal(grid,0,0,grid,0,0)
 
-# pp.pprint(grid)
 
 ################ DISPLAY
 
 
-pygame.init()
-SCREENX = 800
-SCREENY = 800
 
-x_bit = SCREENX / len(grid)
-y_bit = SCREENY / len(grid[0])
 
-#get colour list
-
-items = {}
-for col,column in enumerate(grid):
-    for row,item in enumerate(column):
-        if not item in items:
-            items[item] = 0
-        items[item] = items[item] + 1
-
-#To a 0-1 scale
-ma,mi = MaxMinKeys(items)
-ma = items[ma]
-mi = items[mi]
-span = ma - mi
-for x in items:
-    items[x] = (((items[x] - mi) * 765) / span)
+if output_type == 0:
+    print '"x"',",",'"y"',",",'"value"'         #column headers
+    for col,column in enumerate(grid):
+        for row,item in enumerate(column):
+            print col, ",",row,",",item
 
 
 
-# print '"x"',",",'"y"',",",'"value"'
-# for col,column in enumerate(grid):
-#     for row,item in enumerate(column):
-#         print col, ",",row,",",item
+if output_type == 1:
+    pygame.init()
+    SCREENX = 800
+    SCREENY = 800
+
+    x_bit = SCREENX / len(grid)
+    y_bit = SCREENY / len(grid[0])
+
+    # Heatmap 
+
+    #get colour list
+    items = {}
+    for col,column in enumerate(grid):
+        for row,item in enumerate(column):
+            #if not item in items:
+                #items[item] = item
+            items[item] = item #+ 1
+
+    print grid
+    print items
+
+    # Find max numbers & convert to a 0-1 scale
+    ma,mi = MaxMinKeys(items)
+    ma = items[ma]
+    mi = items[mi]
+    span = ma - mi
+    for x in items:
+        #print items[x]
+        print "x", x
+        print items[x]
+        print mi,ma
+        items[x] = (((items[x] - mi) * 765) / span)
+
+    print items
+
+    screen = pygame.display.set_mode((SCREENX, SCREENY))
+    pygame.display.set_caption('Overhead View')
+    while True: # main game loop
+        for col,column in enumerate(grid):
+            for row,item in enumerate(column):
+                val = items[item]
+                if val <= 255:
+                    pygame.draw.rect(screen, (0,val,255), (x_bit*row,y_bit*col, x_bit, y_bit))
+                elif val <= 510:
+                    pygame.draw.rect(screen, (0,255,val-255), (x_bit*row,y_bit*col, x_bit, y_bit))
+                elif val <= 765:    
+                    pygame.draw.rect(screen, (255,val-510,0), (x_bit*row,y_bit*col, x_bit, y_bit))
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+        pygame.display.update()
 
 
+if output_type == 2:
+    import numpy
+    import matplotlib.pyplot as plt
+    from mpl_toolkits.mplot3d import Axes3D
+    from matplotlib import cm
 
-print "done"
+    # Set up grid and test data
+    nx, ny = 256, 1024
+    x = range(len(grid))
+    y = range(len(grid))
 
-import numpy
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import cm
-# Set up grid and test data
-nx, ny = 256, 1024
-x = range(len(grid))
-y = range(len(grid))
+    data = numpy.array(grid)
 
-data = numpy.array(grid)
+    hf = plt.figure()
+    ha = hf.add_subplot(111, projection='3d')
 
-hf = plt.figure()
-ha = hf.add_subplot(111, projection='3d')
+    X, Y = numpy.meshgrid(x, y)  # `plot_surface` expects `x` and `y` data to be 2D
+    ha.plot_surface(X, Y, data,shade=True,cmap=cm.terrain,rstride=1, cstride=1,)
 
-X, Y = numpy.meshgrid(x, y)  # `plot_surface` expects `x` and `y` data to be 2D
-ha.plot_surface(X, Y, data,shade=True,cmap=cm.terrain,rstride=1, cstride=1,)
-
-plt.show()
-
-# screen = pygame.display.set_mode((SCREENX, SCREENY))
-# pygame.display.set_caption('Hello World!')
-# while True: # main game loop
-#     for col,column in enumerate(grid):
-#         for row,item in enumerate(column):
-#             val = items[item]
-#             if val <= 255:
-#                 pygame.draw.rect(screen, (0,val,255), (x_bit*row,y_bit*col, x_bit, y_bit))
-#             elif val <= 510:
-#                 pygame.draw.rect(screen, (0,255,val-255), (x_bit*row,y_bit*col, x_bit, y_bit))
-#             elif val <= 765:    
-#                 pygame.draw.rect(screen, (255,val-510,0), (x_bit*row,y_bit*col, x_bit, y_bit))
-#     for event in pygame.event.get():
-#         if event.type == QUIT:
-#             pygame.quit()
-#             sys.exit()
-#     pygame.display.update()
-
+    plt.show()
